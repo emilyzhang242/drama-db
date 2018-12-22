0450a9c455eeba6f796b374439daf67ea165e198
Emily Zhang <ez86@cornell.edu>
2018-12-21 23:08:12 -0700

    gulp is working, added package.json for gulp dependencies

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # drama-db
 
+<h2>INSTALLATION INSTRUCTIONS</h2>
+<h3>Installing GULP</h3>
+<ul>
+<li>Create gulpfile.js according to <a href="https://stackoverflow.com/questions/38937095/no-gulpfile-found">these</a> instructions.</li>
+<li>npm install gulp</li>
+<li>pip install django-gulp</li>
+<li>npm install all of the requirements in the gulpfile.js file like <a href="https://stackoverflow.com/questions/43586635/how-to-install-all-required-modules-from-gulpfile-js">this</a>.</li>
+</ul>
+<p>Please note, gulp must be version 4.x or it won't work! Follow instructions <a href="https://github.com/pattern-lab/edition-node-gulp/wiki/Updating-to-Gulp-4">here</a> to uninstall previous version and install new version.</p>
 <h2>RUNNING ENVIRONMENT</h2> 
 <h3>Run Website</h3>
 <ul>
@@ -13,7 +22,7 @@
 <li>python manage.py migrate</li>
 </ul>
 
-<h2>Resources</h2>
+<h2>RESOURCES</h2>
 <p>Setting up the <a href="https://virtualenv.pypa.io/en/latest/userguide/">virtual environment</a>
 <p>Postgres app <a href="https://postgresapp.com/">here</a>
 <p>Setting up Postgres and making migrations <a href="https://medium.com/agatha-codes/painless-postgresql-django-d4f03364989">here</a>
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp');
+
+var clean           = require('gulp-clean');
+var concat          = require('gulp-concat');
+var imagemin        = require('gulp-imagemin');
+var jshint          = require('gulp-jshint');
+var minifycss       = require('gulp-clean-css');
+var plumber         = require('gulp-plumber');
+var rename          = require('gulp-rename');
+var sass            = require('gulp-sass');
+var stylish         = require('jshint-stylish');
+var uglify          = require('gulp-uglify');
+var declare         = require('gulp-declare');
+var shell           = require('gulp-shell');
+
+// compile SASS files
+gulp.task('style', function(){
+  return gulp.src('../scripts/sass/*.scss')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('../static/css'));
+});
+
+// minify and combine 3rd party scripts into one.
+gulp.task('script-lib', function(){
+  return gulp.src('../scripts/javascript/libraries/*.js')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(concat('lib.js'))
+    .pipe(rename('lib.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('../static/js'));
+});
+
+// minify custom scripts used in the website.
+gulp.task('script-custom', function(){
+  return gulp.src('../scripts/javascript/custom/*.js')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('../static/js'));
+});
+
+gulp.task('script', ['script-lib', 'script-custom'], function(){});
+
+gulp.task('watch', function(){
+  gulp.watch('../scripts/sass/**/*.scss', ['style']);
+  gulp.watch('../scripts/javascript/custom/*.js', ['script']);
+  gulp.watch('../scripts/javascript/libraries/*.js', ['script']);
+});
+
+gulp.task('default', ['style', 'script', 'watch'], function(){
+  console.log('waiting...');
+});
