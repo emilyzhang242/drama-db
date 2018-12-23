7893bd665295b41d6f183a28033e1b053b61b8dd
Emily Zhang <ez86@cornell.edu>
2018-12-22 18:11:54 -0700

    profile sidebar mostly in place, chevron css done

--- profile/models.py
+++ profile/models.py
@@ -4,6 +4,6 @@ class Users(models.Model):
 	username = models.CharField(max_length=200, null=False)
 	email = models.CharField(max_length=200, null=False)
 	password = models.CharField(max_length=200, null=False)
-	followed_actors = models.ManyToManyField('actors.actors', related_name="followed_actors")
+	followed_actors = models.ManyToManyField('actors.actors', related_name="followedactors")
 
 
