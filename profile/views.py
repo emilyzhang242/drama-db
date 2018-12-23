c0366b47d68b49dddea8cb5e3098c89ce983fe21
Emily Zhang <ez86@cornell.edu>
2018-12-23 09:39:16 -0700

    login logout basics implemented

--- profile/views.py
+++ profile/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
+from django.contrib.auth.models import User
 
 def newsfeed(request):
 
 	if not request.user.is_authenticated: 
 		return redirect('sign-up')
 	else:
-		User = request.user
-        profile = Users.objects.get(user = User)
-        parameters = {
-            'user':User,
-            'profile':profile,
+		#User = request.user
+        #profile = User.objects.get(user = User)
+        	parameters = {
+            #'user':User,
+            #'profile':profile,
             'page': "profile"
-        }
+        	}
 	return TemplateResponse(
 		request,
 		'profile/newsfeed.html', 
