ee3fab283c451c7bcceaf53a23d89ddd331cbe87
Emily Zhang <ez86@cornell.edu>
2018-12-23 19:51:56 -0700

    scrolling bar is POSSIBLY working, will have to add shows to check

--- scripts/javascript/custom/content.js
+++ scripts/javascript/custom/content.js
@@ -1,2 +1,13 @@
 $(document).ready(function() {
+	//scrollbar 
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		$(".scrollable-point").each(function(f){
+			var id = $(this).attr("id");
+			if (scroll >= $("#"+id).offset().top) {
+				$(".content-nav a").removeClass("active-link");
+				$('a[href$="#'+id+'"]').addClass("active-link");
+			}
+		});
+	});
 });
