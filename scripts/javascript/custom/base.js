86d54587680e22a608c3f90833365c51a6daa096
Emily Zhang <ez86@cornell.edu>
2018-12-25 12:39:19 -0600

    tooltip working

--- scripts/javascript/custom/base.js
+++ scripts/javascript/custom/base.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
 	var page = $("#navbar-choice").val();
 	$(".navbar-highlight").removeClass("navbar-active");
    	$("#navbar-"+page).siblings(".navbar-highlight").addClass("navbar-active");
+
+   	//tooltip
+   	$("i").tooltip({content:"test", tooltipClass: "tooltip-css"});
+
 });
