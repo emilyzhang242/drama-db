c3e890748496a019070d6e788ec793a2cfcbe75c
Emily Zhang <ez86@cornell.edu>
2018-12-25 14:09:03 -0600

    logic bringing faved and followed actors to profile works

--- profile/views.py
+++ profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
 from django.contrib.auth.models import User
+from profile.models import UserProfile
+from actors.models import Actors
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url = 'login')
@@ -25,11 +27,22 @@ def sign_up(request):
 @login_required(login_url = 'login')
 def people(request, filter):
 
+	profile = UserProfile.objects.get(user_id = request.user.id)
+	info = {"both":[], "followed":[], "favorited":[]}
+
+	for actor in Actors.objects.all():
+		if profile.followed_actors.filter(id=actor.id).exists() and profile.favorited_actors.filter(id=actor.id).exists():
+			info["both"].append(actor)
+		elif profile.followed_actors.filter(id=actor.id).exists(): 
+			info["followed"].append(actor)
+		elif profile.favorited_actors.filter(id=actor.id).exists(): 
+			info["favorited"].append(actor)
 
 	parameters = {
 		'page': "profile",
 		'filter': filter, 
-		'profile_page': "People"
+		'profile_page': "People",
+		'actor_info': info
 	}
 	return TemplateResponse(
 		request,
