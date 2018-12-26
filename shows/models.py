99204ab17d567b74f594410bfe32a1ae2835ad57
Emily Zhang <ez86@cornell.edu>
2018-12-25 23:00:49 -0600

    show buttons in place

--- shows/models.py
+++ shows/models.py
@@ -7,7 +7,8 @@ from actors.models import Actors
 class Shows(models.Model): 
 	title = models.CharField(max_length=300, null = True)
 	year = models.IntegerField(null=True)
-	followers = models.IntegerField(null=False, default=0)
+	follower_count = models.IntegerField(null=False, default=0)
+	favorited_count = models.IntegerField(null=False, default=0)
 	url = models.CharField(max_length=200, null=True)
 	page_visits = models.IntegerField(default=0)
 
