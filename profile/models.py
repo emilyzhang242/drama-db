a3c5214a7f19cc2fb6d821a31fb11c1ebab2154a
Emily Zhang <ez86@cornell.edu>
2018-12-27 15:28:57 -0600

    created db for user viewings

--- profile/models.py
+++ profile/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from shows.models import Shows
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model): 
@@ -7,5 +8,19 @@ class UserProfile(models.Model):
 	favorited_actors = models.ManyToManyField('actors.actors', related_name="favoritedactors")
 	followed_shows = models.ManyToManyField('shows.shows', related_name="followedshows")
 	favorited_shows = models.ManyToManyField('shows.shows', related_name="favoritedshows")
+
+class ShowViews(models.Model):
+
+	STATUS_CHOICES = (
+		('C', "Completed"),
+		('W', "Watching"),
+		('Q', "Queued")
+		)
+
+	show = models.ForeignKey(Shows)
+	user = models.ForeignKey(UserProfile)
+	start_date = models.DateField(null=True, auto_now_add=True)
+	end_date = models.DateField(null=True)
+	status = models.CharField(max_length=20, choices=STATUS_CHOICES)
 
 
